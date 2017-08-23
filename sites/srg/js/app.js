f176b7495b9294685bcc58b36113f0dcc9014dd9
yadobr <php.dev@mail.ru>
2017-08-24 00:12:58 +0800

    sites/srg2

--- sites/srg/js/app.js
+++ sites/srg/js/app.js
@@ -2,9 +2,9 @@ $(document).ready(function()
 {
 	var appId = '9B546185-DF31-5378-FF41-1FB6010ECE00',
     secretKey = 'E5BE339C-7642-26AD-FF57-19404DDA1D00',
-    version = 'v1'; 
+    version = 'v1';
 
-	Backendless.initApp( appId, secretKey, version); // where to get the argument values for this call  	
+	Backendless.initApp( appId, secretKey, version); // where to get the argument values for this call
 });
 
 function send(hnd)
@@ -24,30 +24,30 @@ function send(hnd)
 
 			setTimeout(function()
 				{
-					$(hnd).html('Отправить');			
+					$(hnd).html('Отправить');
 				}, 4000);
 
 			Backendless.UserService.login( 'test@mail.ru', 'testpassw');
 
-			var successCallback = function( response ) 
+			var successCallback = function( response )
 			{
 			  alert('Заявка принята! Наш мастер скоро перезвонит вам!');
 			};
-			 
-			var failureCallback = function( fault ) 
+
+			var failureCallback = function( fault )
 			{
 			  alert('Ошибка отправки. Пожалуйста, свяжитесь с нами по электронной почте: remontuu03@mail.ru');
 			};
-			 
+
 			// prepare message bodies (plain and html) and attachment
 			var bodyParts = new Bodyparts();
 			bodyParts.textmessage = $('#txt').val();
 			bodyParts.htmlmessage = 'Имя: <b>'+name+'</b><br>Номер: <b>'+num+'</b>';
 			//var attachments = ["backendless-codegen.zip" ];
-			 
+
 			// asynchronous call
 			var responder = new Backendless.Async( successCallback, failureCallback );
-			Backendless.Messaging.sendEmail( "Заявка", bodyParts, [ "remontuu03@mail.ru" ], undefined, responder );
+			//Backendless.Messaging.sendEmail( "Заявка", bodyParts, [ "remontuu03@mail.ru" ], undefined, responder );
 
 		}
 		else
@@ -59,7 +59,7 @@ function send(hnd)
 	{
 		alert('Поля "Имя" и "Номер телефона" не должны быть пустыми!');
 	}
-} 
+}
 
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : evt.keyCode;
